20c8661c23528da81883a33b7f9db22ca049904e
Michel Casabianca <mcasabianca@sfbx.io>
2024-02-06 16:27:05 +0100

    Implemented

---
+++ README.md
@@ -0,0 +1,49 @@
+# Lone Command
+
+Lone command is a way to ensure two instances of the same command won't run
+at the same time.
+
+## Installation
+
+### Unix users (Linux, BSDs and MacOSX)
+
+Unix users may download and install latest *lone* release with command:
+
+```bash
+sh -c "$(curl https://sweetohm.net/dist/lone/install)"
+```
+
+If *curl* is not installed on you system, you might run:
+
+```bash
+sh -c "$(wget -O - https://sweetohm.net/dist/lone/install)"
+```
+
+**Note:** Some directories are protected, even as *root*, on **MacOSX** (since *El Capitan* release), thus you can't install *lone* in */usr/bin* for instance.
+
+### Binary package
+
+Otherwise, you can download latest binary archive at <https://github.com/c4s4/lone/releases>. Unzip the archive, put the binary of your platform somewhere in your *PATH* and rename it *lone*.
+
+## Usage
+
+To ensure that command *cmd args* only runs once at a time, you would type:
+
+```bash
+lone 12345 cmd args
+```
+
+Where:
+
+- *12345* is a port number that should be the same for given command. Must be
+  greater than 1024 if not running as root.
+- *cmd args* is the command to run with arguments.
+
+This command will:
+
+- Open a server socket on given port *12345*. So that if another lone command
+  is already listening this port, this will fail.
+- Run given command.
+- Release the port when done.
+
+*Enjoy!*
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use clap::Parser;
+use std::env;
+use std::net::TcpListener;
+use std::process;
+use std::process::Command;
+use std::thread;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// Run command ensuring only one instance is running on this system
+#[derive(Parser)]
+struct Cli {
+    /// The lone version
+    #[arg(short, long)]
+    version: bool,
+    /// Run command in a shell
+    #[arg(short, long, default_value_t = false)]
+    shell: bool,
+    /// The port to use
+    port: u16,
+    /// The command to run
+    cmd: Vec<String>,
+}
+
+fn main() {
+    // parse command line arguments
+    let args = Cli::parse();
+    // print version and exit
+    if args.version {
+        println!("{}", VERSION);
+        return;
+    }
+    // check command is not empty
+    if args.cmd.is_empty() {
+        return;
+    }
+    // open tcp server on port
+    let mut address = String::from("127.0.0.1:");
+    address.push_str(&args.port.to_string());
+    let result = TcpListener::bind(address);
+    if result.is_err(){
+        eprintln!("ERROR: port {:?} already in use", args.port);
+        process::exit(1);
+    }
+    // run listener in a separate thread
+    thread::spawn(move || {
+        let listener = result.unwrap();
+        for _ in listener.incoming() {
+        }
+    });
+    if args.shell {
+        // run command
+        if env::consts::OS == "windows" {
+            // on windows
+            if let Err(err) = Command::new("cmd")
+                .arg("/c")
+                .arg(&args.cmd.join(" "))
+                .status()
+            {
+                eprintln!("ERROR running command: {err}");
+                process::exit(1);
+            };
+        } else {
+            // on unix
+            if let Err(err) = Command::new("sh")
+                .arg("-c")
+                .arg(&args.cmd.join(" "))
+                .status()
+            {
+                eprintln!("ERROR running command: {err}");
+                process::exit(1);
+            };
+        }
+    } else {
+        // run command
+        if let Err(err) = Command::new(&args.cmd[0]).args(&args.cmd[1..]).status() {
+            eprintln!("ERROR running command: {err}");
+            process::exit(1);
+        };
+    }
+}
