7186ba330464d293f79c78b5544d8b482e0a7f5a
Michel Casabianca <mcasabianca@sfbx.io>
2024-04-16 11:35:31 +0200

    Running command in shell

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,18 @@ use std::net::TcpListener;
 use std::process;
 use std::process::Command;
 use std::thread;
+use anyhow::{Context, Result};
+
+const DEFAULT_SHELL_UNIX: &str = "sh";
+const DEFAULT_SHELL_WINDOWS: &str = "cmd";
 
 /// Run command ensuring only one instance is running on this system
 #[derive(Parser)]
 #[command(version)]
 struct Cli {
-    /// Run command in a shell
-    #[arg(short, long, default_value_t = false)]
-    shell: bool,
+    /// Shell to run command in (default: sh on Unix or cmd on Windows)
+    #[clap(short, long)]
+    shell: Option<String>,
     /// The port to use
     port: u16,
     /// The command to run
@@ -21,56 +25,47 @@ struct Cli {
 fn main() {
     // parse command line arguments
     let args = Cli::parse();
+    match run(args) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("ERROR {:#}", e);
+            process::exit(1);
+        },
+    }
+}
+
+fn run(args: Cli) -> Result<()> {
     // check command is not empty
     if args.cmd.is_empty() {
-        return;
+        ()
     }
     // open tcp server on port
     let address = format!("127.0.0.1:{}", args.port.to_string());
-    let listener = match TcpListener::bind(address) {
-        Ok(listener) => listener,
-        Err(err) => {
-            eprintln!("ERROR binding port {}: {err}", args.port);
-            process::exit(1);
-        }
-    };
+    let listener = TcpListener::bind(address)
+        .with_context(|| format!("binding port {}", args.port))?;
     // run listener in a separate thread
     thread::spawn(move || for _ in listener.incoming() {});
     // run command
-    process::exit(run_command(args.cmd, args.shell));
+    let exit_code = run_command(args.cmd, args.shell).context("running command")?;
+    process::exit(exit_code);
 }
 
 /// Run command
-fn run_command(cmd: Vec<String>, shell: bool) -> i32 {
-    if shell {
-        // run command in shell
-        if env::consts::OS == "windows" {
-            // on windows
-            match Command::new("cmd").arg("/c").arg(&cmd.join(" ")).status() {
-                Ok(status) => return status.code().unwrap(),
-                Err(err) => {
-                    eprintln!("ERROR running command: {err}");
-                    process::exit(1);
-                }
-            }
-        } else {
-            // on unix
-            match Command::new("sh").arg("-c").arg(&cmd.join(" ")).status() {
-                Ok(status) => return status.code().unwrap(),
-                Err(err) => {
-                    eprintln!("ERROR running command: {err}");
-                    process::exit(1);
-                }
-            }
+fn run_command(cmd: Vec<String>, shell: Option<String>) -> Result<i32> {
+    // run command in shell
+    if env::consts::OS != "windows" {
+        // on unix
+        let shell = shell.unwrap_or(DEFAULT_SHELL_UNIX.to_string());
+        match Command::new(shell).arg("-c").arg(&cmd.join(" ")).status() {
+            Ok(status) => return Ok(status.code().unwrap()),
+            Err(err) => anyhow::bail!(err),
         }
     } else {
-        // run command without shell
-        match Command::new(&cmd[0]).args(&cmd[1..]).status() {
-            Ok(status) => return status.code().unwrap(),
-            Err(err) => {
-                eprintln!("ERROR running command: {err}");
-                process::exit(1);
-            }
+        // on windows
+        let shell = shell.unwrap_or(DEFAULT_SHELL_WINDOWS.to_string());
+        match Command::new(shell).arg("/c").arg(&cmd.join(" ")).status() {
+            Ok(status) => return Ok(status.code().unwrap()),
+            Err(err) => anyhow::bail!(err),
         }
     }
 }
